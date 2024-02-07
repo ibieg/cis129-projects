f63b5ec6583e2bebc965226fc25397fb505d194e
ibieg <158854639+ibieg@users.noreply.github.com>
2024-02-07 10:07:06 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -4,4 +4,4 @@
 print('Timmy was walking down the sidewalk.')
 print('Timmy saw a frog up ahead and went to grab it.')
 print('After he grabbed it someone approached Timmy.')
-print('The man said "By the way that\'s a toad not a frog".')
+print('The man said "By the way that\'s a toad not a frog."')
