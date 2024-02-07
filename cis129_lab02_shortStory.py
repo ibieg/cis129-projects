1e0991dec2f072d9dbb8eea26a05ec0b7edae142
ibieg <158854639+ibieg@users.noreply.github.com>
2024-02-07 10:06:13 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,7 +1,7 @@
 #Ian Bieg wrote this
 #Tells a story to user
 #lengthened the story
-print("Timmy was walking down the sidewalk.")
-print("Timmy saw a frog up ahead and went to grab it.")
-print("After he grabbed it someone approached Timmy.")
-print("The man said 'By the way that's a toad not a frog'.")
+print('Timmy was walking down the sidewalk.')
+print('Timmy saw a frog up ahead and went to grab it.')
+print('After he grabbed it someone approached Timmy.')
+print('The man said "By the way that\'s a toad not a frog".')
