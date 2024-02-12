c688476c59f6635235e4e316bf10079ca0201533
ibieg <158854639+ibieg@users.noreply.github.com>
2024-02-12 08:36:04 -0700

    Add files via upload

--- cis129_lab03_coffeeshop.py
+++ cis129_lab03_coffeeshop.py
@@ -17,8 +17,6 @@ while True:
         else:
             print('Please put in a positive number')
                 
-                
-              
 print('How many muffins would you like?')
 while True:
     try:
@@ -31,18 +29,45 @@ while True:
         else:
             print('Please put in a positive number')
 
+print('How many 55ft Yeti would you like?')
+while True:
+    try:
+        yetiQuantity = int(input())
+    except:
+        print('Please put in a positive number')
+    else:
+        if yetiQuantity >= 0:
+            break
+        else:
+            print('Please put in a positive number')
+
+print('How many horeshoes would you like?')
+while True:
+    try:
+        shoeQuantity = int(input())
+    except:
+        print('Please put in a positive number')
+    else:
+        if shoeQuantity >= 0:
+            break
+        else:
+            print('Please put in a positive number')
+
 print('***********************************\n\n')
 print('***********************************')
 
-#Prices and Tax
+#Prices and Tax variables
 
 salesTax = float(.06)
 coffeePrice = float(5.00)
 muffinPrice = float(4.00)
+yetiPrice = float(5500)
+shoePrice = float(9.50)
 
 #Total price calculation
 
-taxlessPrice = coffeeQuantity * coffeePrice + muffinQuantity * muffinPrice
+taxlessPrice = coffeeQuantity * coffeePrice + muffinQuantity * muffinPrice +\
+yetiPrice * yetiQuantity + shoePrice * shoeQuantity
 
 taxPrice = taxlessPrice * salesTax
 
@@ -56,6 +81,12 @@ print(coffeeQuantity,'coffee at $' + str(coffeePrice), 'each: '
 
 print(muffinQuantity,'muffin at $' + str(muffinPrice), 'each: '
       '$' + str(muffinQuantity * muffinPrice))
+
+print(yetiQuantity, '55ft Yeti at $' + str(yetiPrice), 'each: '
+      '$' + str(yetiQuantity * yetiPrice))
+
+print(shoeQuantity, 'horseshoes at $' + str(shoePrice), 'each: '
+      '$' + str(shoeQuantity * shoePrice))
 
 print(str(salesTax * 100) + '% tax: $' + str(taxPrice))
 
