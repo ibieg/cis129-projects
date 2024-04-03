4b36f1ee361cac61afb16d421ddeca5095014aff
ibieg <158854639+ibieg@users.noreply.github.com>
2024-04-03 10:16:29 -0700

    Add files via upload

---
+++ cis129_IanBieg_lab08_palindrome.py
@@ -0,0 +1,44 @@
+#ian bieg  
+#determines if a word or expression can be considered a palindrome
+def main():
+    SENTINEL = 'q'
+    palindrome = str()
+    #loop question
+    while palindrome != SENTINEL:
+        palindrome = input('Enter a possible palindrome ("q" to quit)\n')
+        if palindrome != SENTINEL:
+            isPalindrome(palindrome)
+    print('Goodbye')
+
+#determine if a word or expression can be considered a palindrome
+def isPalindrome(palindrome):
+        palindromeList = []
+        #remove formatting characters for numbers
+        modPalindrome = palindrome.replace(' ','')
+        modPalindrome = modPalindrome.replace(',','')
+        modPalindrome = modPalindrome.replace('.','')
+        #create list of letters in palindrome(lowercase)
+        palindromeList += modPalindrome.lower()
+        #triggers if user enters nothing
+        if modPalindrome == '':
+             print('The nothingness awaits')
+             return
+        #triggers if word or expression is a questionable palindrome
+        if len(palindromeList) == 1:
+             print('Can a singular letter or number be considered a palindrome?')
+        #checks if the last letter of the word is the same as the first until the middle
+        print('made it here')
+        for item in range(len(palindromeList)//2):
+            if palindromeList.pop() == palindromeList[item]:
+                 if item == len(palindromeList)//2:
+                      print(f'{palindrome} is a palindrome')
+                 continue
+            else:
+                 print(f'{palindrome} is not a palindrome')
+                 break
+        print('made it there')
+
+
+
+
+main()
